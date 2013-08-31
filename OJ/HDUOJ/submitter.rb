1c5c3d755e4f8b62ceacd7bbbf8fa4828eb39785
footo <footo@outlook.com>
2013-08-31 16:33:13 +0800

    Improving HDU submitter

--- OJ/HDUOJ/submitter.rb
+++ OJ/HDUOJ/submitter.rb
@@ -2,6 +2,7 @@ require 'net/http'
 
 def my_post(uri,data)
 	cookie = "PHPSESSID=dmgqajp0bp0o4f3r3eu183l8v2"
+	cookie = "exesubmitlang=0; PHPSESSID=nr6bcri83c46l7353b7o2omem7"
 	req = Net::HTTP::Post.new(uri.path)
 	req.set_form_data(data)
 	req['Cookie']=cookie
@@ -11,18 +12,32 @@ def my_post(uri,data)
 	end
 	res
 end
-=begin
+
+def my_get(uri)
+	http=Net::HTTP.new('acm.hdu.edu.cn')
+	cookie = "PHPSESSID=nr6bcri83c46l7353b7o2omem7"
+	#uri = URI('http://poj.org/showsource?solution_id='+id.to_s)
+	#data = 'solution_id='+id.to_s
+	headers = { 'Cookie'=>cookie }
+	res=http.get(uri,headers).body
+end
+
 def search(content,searchmode='title')
-	uri = URI('http://acm.hdu.edu.cn/search.php?action=listproblem')
+	uri = URI('http://acm.hdu.edu.cn/search.php?field=problem&key='+
+		content+'&searchmode='+searchmode)
+=begin
 	data = {
 		#'action'=>'listproblem',
-		'content'=>content,
+		'field'=>'problem',
+		'key'=>content,
 		'searchmode'=>searchmode
+		#searchmode=>1
 	}
-	my_post(uri,data)
+=end
+	my_get(uri)
 #	Net::HTTP.post(uri,data)
 end
-=end
+
 def get_run_id(id,language=0)
 	uri = URI('http://poj.org/status?problem_id='+id.to_s+
 		'&user_id=submittersubmitter&language='+language.to_s)
@@ -39,9 +54,10 @@ def submit(id,path,language=0)
 
 	uri = URI('http://acm.hdu.edu.cn/submit.php')
 	data = {
-		'check'=>0,
-		'problemid'=>id,
-		'language'=>language,
+		'action'=>'submit',
+		'check'=>'0',
+		'problemid'=>id.to_s,
+		'language'=>language.to_s,
 		'usercode'=>code
 	}
 	my_post(uri,data)
@@ -69,6 +85,12 @@ def trace_status(id)
 	status
 end
 
-#puts search("shit").body
+def tttt
+	uri = URI('http://acm.hdu.edu.cn/submit.php?pid=4576')
+	my_get(uri)
+end
+
+#puts search("shit")
 puts submit(4576,"tp.cpp")
+#puts tttt
 #puts trace_status(12024682)
